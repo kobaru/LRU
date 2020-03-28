fbeae0f74e3db2222998eaaf466bdee08d12c4fb
Your Name <you@example.com>
2020-03-28 16:22:28 +0900

    Map追加

--- LRUTest.java
+++ LRUTest.java
@@ -1,10 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LRUTest {
 
     @Test
     public void Mapを使えるようにする() {
-        
+        Map<String, String> lru = new LRU<>();
     }
 }
